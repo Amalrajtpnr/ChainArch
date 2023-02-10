ef53e57a7eb8bcbf861f036f0d2b53944914c22e
Rahul Mohan <rahulmohanav35@gmail.com>
2023-02-10 15:43:08 +0530

    homepage

---
+++ pages/newtask/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='h-[100vh] w-[100vw] bg-[#000000] absolute flex overflow-hidden '>
+
+<div className=" h-[50px] w-[50px] bg-[#26E5FF] rounded-full absolute -top-28 opacity-30  overflow-hidden left-[100%]  shadow-[0px_0px_990px_350px_rgba(0,0,0,0.3)] shadow-[#26E5FF]  "></div>
+      <div className=" h-[10px] w-[10px] bg-[#26E5FF] rounded-full absolute top-72 opacity-30  shadow-[0px_0px_890px_150px_rgba(0,0,0,0.3)] shadow-[#26E5FF]   overflow-hidden -left-16 "></div>
+<div className=" h-[100vh] w-[100vw] bg-transparent fixed top-0 z-100 backdrop-blur-[10px] flex justify-between items-center">
+
+<div className="h-[85%] w-[40%] border-2 border-yellow-500"></div>
+
+</div>
+    </div>
+  )
+}
+
+export default index;
