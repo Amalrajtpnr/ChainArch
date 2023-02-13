7c40ff6e20bbbdc7770a316ae36c6bb2aad4abd1
Rahul Mohan <rahulmohanav35@gmail.com>
2023-02-13 09:45:49 +0530

    newdesign

--- pages/newtaskapi/index.tsx
+++ pages/newtaskapi/index.tsx
@@ -6,40 +6,84 @@ function index() {
     <div className="h-[100vh] w-[100vw] bg-[#000000] absolute flex overflow-hidden">
       <div className=" h-[50px] w-[50px] bg-[#26E5FF] rounded-full absolute -top-28  opacity-40  overflow-hidden left-[95%]  shadow-[0px_0px_790px_350px_rgba(0,0,0,0.3)] shadow-[#B200FF]"></div>
       {/* <div className=" h-[10px] w-[10px] bg-[#26E5FF] rounded-full absolute top-72 opacity-90  shadow-[0px_0px_890px_150px_rgba(0,0,0,0.3)] shadow-[#B200FF]   overflow-hidden -left-16 "></div> */}
-      <div className=" h-[100vh] w-[100vw] bg-transparent fixed top-0 z-100 backdrop-blur-[10px] flex justify-between items-center">
-        <div className="h-[87%] w-[37%] border-2 border-[#573f61] rounded-3xl ml-12 bg-gradient-to-tr from-[#02021c2e] via-[#08083a78]  to-[#02021c2e] flex justify-evenly items-center flex-col shadow-shade ">
-          <h1 className=" font-ice  mr-60 -mt-14  font-normal text-5xl text-white tracking-wider">
-            New Task
+      <div className=" h-[100vh] w-[100vw] bg-transparent fixed top-0 z-100 backdrop-blur-[10px] flex justify-center items-center">
+        <div className="h-[75%] w-[40%]  flex items-center justify-center flex-col  mr-36 mt-10">
+          <h1 className="text-3xl tracking-wider text-white font-inter font-bold mr-[70px] mb-3">
+            Create Task
           </h1>
+          <Image width={260} height={260} src={"/Frame.svg"} alt="" />
+          <h1 className="text-white w-[80%] text-start tracking-wider ml-40 my-5">
+            Streamlining Daily Tasks with Automation for Increased Efficiency
+            and Productivity
+          </h1>
+        </div>
 
-          <div className="h-[60px] w-[90%] flex items-center justify-between flex-col relative -mt-10 ">
-            <div className="h-[26px] w-auto text-[11px]  px-1  bg-[#040417] text-white z-100 mr-80  absolute -top-[12px] text flex items-center justify-center ">
+        <div className="h-[85%] w-[37%]  rounded-xl  flex justify-evenly items-start flex-col mt-12">
+          <div className="h-[75px] w-[85%] flex items-start justify-between flex-col relative  ">
+            <h1 className="text-white text-sm font-bold my-1">
               Contact Address
-            </div>
+            </h1>
             <input
-              className=" h-[100%] w-[100%] text-sm pl-3 box-border bg-[#040417] flex border-2 border-[#573f61] text-white rounded-xl"
+              className=" h-[100%] w-[100%] pl-3 box-border bg-[#0E0E0E] flex  text-sm text-white rounded-xl"
               type="text"
               placeholder="ba"
             />
           </div>
 
-          <div className="h-[150px] w-[90%] flex items-center justify-between flex-col relative -mt-16 ">
-            <div className="h-[26px] w-auto text-[11px] px-1  bg-[#040417] text-white z-100 mr-96 absolute -top-[12px] text flex items-center justify-center ">
-              ABI
-            </div>
+          <div className="h-[150px] w-[85%] flex items-start justify-between flex-col relative">
+            <h1 className="text-white text-sm font-bold my-1">ABI</h1>
             <textarea
-              className="h-[100%] w-[100%] pl-3 text-sm box-border bg-[#040417] flex border-2 border-[#573f61] text-white rounded-xl pt-6 "
-              name="abi"
+              className="h-[100%] w-[100%] bg-[#0E0E0E]  flex  text-sm text-white rounded-xl  pl-3 pt-3 box-border"
+              name="ABI"
               id=""
               cols={30}
               rows={10}
-              placeholder="abc"
+              placeholder="Abc"
             ></textarea>
           </div>
-        </div>
 
-        <div className="h-[87%] w-[37%]  flex items-center justify-center mr-36">
-          <Image width={400} height={400} src={"/Frame.svg"} alt="" />
+          <div className="h-[10%] w-[85%] flex items-center justify-between">
+            <div className="h-[75px] w-[46%] flex items-start justify-between flex-col relative  ">
+              <h1 className="text-white text-sm font-bold my-1">Task Name</h1>
+              <input
+                className=" h-[100%] w-[100%] pl-3 box-border bg-[#0E0E0E] flex  text-sm text-white rounded-xl"
+                type="text"
+                placeholder="ba"
+              />
+            </div>
+
+            <div className="h-[75px] w-[46%] flex items-start justify-between flex-col relative  ">
+              <h1 className="text-white text-sm font-bold my-1">
+                Initial Amount
+              </h1>
+              <input
+                className=" h-[100%] w-[100%] pl-3 box-border bg-[#0E0E0E] flex  text-sm text-white rounded-xl"
+                type="text"
+                placeholder="ba"
+              />
+            </div>
+          </div>
+
+          <div className="h-[75px] w-[85%] flex items-start justify-between flex-col relative  ">
+            <h1 className="text-white text-sm font-bold my-1">Gas Limit</h1>
+            <input
+              className=" h-[100%] w-[100%] pl-3 box-border bg-[#0E0E0E] flex text-sm text-white rounded-xl"
+              type="text"
+              placeholder="ba"
+            />
+          </div>
+
+          <div className="h-[75px] w-[85%] flex items-start justify-between flex-col relative  ">
+            <h1 className="text-white text-sm font-bold my-1">Interval</h1>
+            <input
+              className=" h-[100%] w-[100%] pl-3 box-border bg-[#0E0E0E] flex  text-sm text-white rounded-xl"
+              type="text"
+              placeholder="ba"
+            />
+          </div>
+          <button className=" h-[7%] w-[25%]  bg-gradient-to-r from-[#592D7C] to-[#260441] border-2 border-[#bb9bd6] rounded-[15px] ml-72 text-white text-sm font-semibold flex justify-center items-center">
+            Create
+          </button>
         </div>
       </div>
     </div>
