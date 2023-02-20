acdee5d338aa38394abdd1a99dc14399eeca8999
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-02-20 11:48:51 +0530

    added mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function Home() {
       </Head>
       <div className=" h-[50px] w-[50px] bg-[#26E5FF] rounded-full absolute -top-28  opacity-40  overflow-hidden left-[95%]  shadow-[0px_0px_790px_350px_rgba(0,0,0,0.3)] shadow-[#B200FF]"></div>
       {/* <div className=" h-[10px] w-[10px] bg-[#26E5FF] rounded-full absolute top-72 opacity-90  shadow-[0px_0px_890px_150px_rgba(0,0,0,0.3)] shadow-[#26E5FF]   overflow-hidden -left-16 "></div> */}
-      <div className="  h-[100vh] w-[100vw] bg-transparent fixed top-0 z-100 backdrop-blur-[10px] hidden lg:flex justify-between items-center">
+      <div className=" h-[100vh] w-[100vw] bg-transparent fixed top-0 z-100 backdrop-blur-[10px] hidden lg:flex justify-between items-center">
         <NavBar />
         <div className="h-[100%] w-[55%]  flex flex-col items-start justify-center pl-5 box-border ml-[100px] ">
           <div className="h-[56%] w-[66%] flex items-center justify-center  -left-7  relative top-4 ">
@@ -83,7 +83,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="relative w-full h-full flex flex-col items-center justify-center p-[5%]">
+      <div className="relative w-full h-full flex lg:hidden flex-col items-center justify-center p-[5%]">
         <NavBar />
         <Image
           className="absolute animate-pulse bottom-[10%]"
